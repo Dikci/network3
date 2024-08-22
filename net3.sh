924f565aaf13471937f61bc45cc5d87fa9ffae8b
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-08-22 17:21:51 +0300

    Update net3.sh

--- net3.sh
+++ net3.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y screen net-tools
 wget https://network3.io/ubuntu-node-v2.1.0.tar
 tar -xvf ubuntu-node-v2.1.0.tar
-screen -S network3
 cd ubuntu-node
 sudo bash manager.sh up
+sudo bash manager.sh key
 sleep 60
